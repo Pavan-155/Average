737c4650d9b12a5c95af486cfab72f2c35f8c931
Pavan-155 <pavankumargangula8@gmail.com>
2025-06-11 12:14:15 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,21 @@
+package basicprogram;
+import java.util.*;
+public class Average {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner (System.in);
+		int a=5,b=10,c=35,d=45,e=50,sum=0;
+		Float Avg;
+		
+		sum = a+b+c+d+e;
+		
+		Avg = sum/5.0f;
+		
+		System.out.println("find the sum ="+sum);
+		System.out.println("find the avg="+Avg);
+		 
+
+	}
+
+}
